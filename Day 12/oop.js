dadf7fd7831af071850e6b953d8807fe10339dbc
Deepak <deepekbhatt455@gmail.com>
2025-02-16 20:42:44 +0530

    oops

---
+++ Day 12/oop.js
@@ -0,0 +1,31 @@
+const user = {
+    usrName: "deepak",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function() {
+        // console.log("Got Details"); 
+        // console.log(`Username: ${this.usrName}`); 
+        // console.log(this);
+           
+    }
+}
+
+// console.log(user.getUserDetails());
+
+
+function User(userName, loginCount, isLoggedIn) {
+    this.userName = userName;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+const userOne = new User("deepak", 12, true);
+const userTwo = new User("Khushi", 10, false);
+console.log(userOne);
+console.log(userTwo);
+
+
+// 7:11 timestamp
---
+++ Day 12/oops.md
@@ -0,0 +1,23 @@
+# Javascript Classes
+
+## OOP
+
+## Object
+- collection of properties and methods
+- toLowerCase
+
+## Why use OOP
+
+## parts if OOP
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+## 4 pillars
+- Abstraction
+- Encapsulation
+- Inheritance
+- Polymorphism
