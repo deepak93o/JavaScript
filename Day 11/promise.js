69c52d1680416fea87fb90ca46801103b84464ce
Deepak <deepekbhatt455@gmail.com>
2025-02-17 21:29:42 +0530

    promises.js

--- Day 11/promise.js
+++ Day 11/promise.js
@@ -56,43 +56,45 @@
     
 // }).finally(() => console.log("The promise is either resolved or rejected"))
 
-const promiseFive = new Promise((resolve, reject) => {
-    setTimeout(function() {
-        let error = true
-        if(!error){
-            resolve({username: "javascript", password: "45516D"})
-        }
-        else {
-            reject('ERROR: Promise Five went wrong');
-        }
-    }, 2000);
-});
+// const promiseFive = new Promise((resolve, reject) => {
+//     setTimeout(function() {
+//         let error = true
+//         if(!error){
+//             resolve({username: "javascript", password: "45516D"})
+//         }
+//         else {
+//             reject('ERROR: Promise Five went wrong');
+//         }
+//     }, 2000);
+// });
 
-async function consumePromiseFive() {
-    try {
-        const response = await promiseFive
-        console.log(response);
-    } catch (error) {
-        console.log(error);
+// async function consumePromiseFive() {
+//     try {
+//         const response = await promiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
         
-    }    
-}
-consumePromiseFive()
+//     }    
+// }
+// consumePromiseFive()
 
-async function getAllUSers() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-// getAllUSers()
+// async function getAllUSers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+//  getAllUSers()
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
     return response.json()
 })
-.then(() =>{})
-.catch((error) => console.log(error)) 
+.then((data) =>{
+    console.log(data);
+})
+.catch((error) => console.log(error));
